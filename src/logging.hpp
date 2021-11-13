263fe7453814a0fd536c4bc2d9f0e81b6418b4d7
Benjamin <mafortion.benjamin@gmail.com>
2021-11-13 01:06:42 +0100

    GLFW, pre-compiled headers

--- src/logging.hpp
+++ src/logging.hpp
@@ -3,13 +3,7 @@
 #include "fmt/core.h"
 #include "fmt/chrono.h"
 
-/* A clever hack (https://stackoverflow.com/a/40947954) to get a project-root relative filepath.
- * SOURCE_DIR_PATH_LENGTH is a macro defined in the CMake script.
- * The cast (from char[]) to char * is needed to suppress string-plus-int warning
- * */
-#define __FILENAME__ ((const char *)__FILE__ + SOURCE_DIR_PATH_LENGTH)
-
-#define LOG(formatstr, ...) __LogInternal("[{:%H:%M:%S}] {}:{}: " formatstr "\n", __FILENAME__, __LINE__, ## __VA_ARGS__)
+#define LOG(formatstr, ...) __LogInternal("[{:%H:%M:%S}] {}:{}: " formatstr "\n", __FILE__ + 3, __LINE__, ## __VA_ARGS__)
 
 template<typename ...Args>
 void __LogInternal(const char *formatstr, const char *file, int line, const Args ...args)
--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,41 @@
-#include "fmt/core.h"
 #include "logging.hpp"
+#include "glad/glad.h"
+#include "GLFW/glfw3.h"
+
+void GlfwErrorCallback(int code, const char *description)
+{
+    LOG("GLFW error {}: {}", code, description);
+}
 
 int main()
 {
-    LOG("Hello, world!");
+    LOG("Launching IT517G");
+
+    if (!glfwInit())
+    {
+        LOG("GLFW initialization failed");
+        return 1;
+    }
+
+    glfwSetErrorCallback(&GlfwErrorCallback);
+
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    GLFWwindow *window = glfwCreateWindow(1024, 768, "Fördjupningsarbete inom spelutveckling", nullptr, nullptr);
+    if (!window)
+    {
+        LOG("Window or OpenGL context creation failed");
+        return 1;
+    }
+
+    glfwMakeContextCurrent(window);
+    gladLoadGL(glfwGetProcAddress);
+
+    while (!glfwWindowShouldClose(window))
+    {
+//        glfwPollEvents();
+        glfwWaitEvents();
+    }
+
     return 0;
 }
